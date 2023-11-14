2c46ce936a85fed170c3d23e19e608749c8c3bf6
binbinweng <32993570+binbinweng@users.noreply.github.com>
2023-11-14 01:15:24 -0800

    Update scheduling.py

--- scheduling/scheduling.py
+++ scheduling/scheduling.py
@@ -7,7 +7,8 @@ from datetime import datetime
 import ast
 import math
 
-def HEFT(EST, EFT, pos, pscheduling, qlist, T, c, E, n, p):
+def HEFT(EST, EFT, pos, pscheduling, qlist, T, c, E, n, p,frequency,processorinfo):
+    EA=0
     for idx, i in enumerate(qlist):
         if idx<pos:
             continue
@@ -24,7 +25,7 @@ def HEFT(EST, EFT, pos, pscheduling, qlist, T, c, E, n, p):
                     EFT[i-1][0]=k
                     EST[i-1][1]=tmp
                     EST[i-1][0]=k
-                    pscheduling[k]=tmp2
+                    #pscheduling[k]=tmp2
             else:
                 pred = E[i-1]
                 tmp = 0
@@ -38,8 +39,10 @@ def HEFT(EST, EFT, pos, pscheduling, qlist, T, c, E, n, p):
                     EFT[i-1][0]=k
                     EST[i-1][1]=tmp
                     EST[i-1][0]=k
-                    pscheduling[k]=tmp2
-    return EFT[qlist[-1]-1][1]
+                    #pscheduling[k]=tmp2
+        EA = EA + (processorinfo[EFT[i-1][0]][0]+processorinfo[EFT[i-1][0]][1]*pow(frequency[EFT[i-1][0]][1],processorinfo[EFT[i-1][0]][2]))*T[i-1][EFT[i-1][0]]
+        pscheduling[EFT[i-1][0]]=EFT[i-1][1]
+    return EA,EFT[qlist[-1]-1][1]
 
 
 def DUPRS1(EST, EFT, pos, pscheduling, qlist, T, c, E, n, p,latency,frequency,processorinfo,proc_freq,sl):
@@ -50,7 +53,8 @@ def DUPRS1(EST, EFT, pos, pscheduling, qlist, T, c, E, n, p,latency,frequency,pr
         proc = EFT[i-1][0]
         proc_min = frequency[proc][0]
         proc_max = frequency[proc][1]
-        rng = np.arange(proc_min, proc_max, 0.05)
+        stp = (proc_max-proc_min)/100
+        rng = np.arange(proc_min, proc_max, stp)
         for f in rng:
             tmp = EST[i-1][1]+T[i-1][proc]*proc_max/f
             tmp2 = (processorinfo[proc][0]+processorinfo[proc][1]*pow(f,processorinfo[proc][2]))*T[i-1][proc]*proc_max/f
@@ -66,8 +70,48 @@ def DUPRS1(EST, EFT, pos, pscheduling, qlist, T, c, E, n, p,latency,frequency,pr
             pscheduling[proc]= EFT[i-1][1]
         else:
             EA = EA+p_energy
-        sl = HEFT(EST,EFT,idx+1,pscheduling,qlist,T,c,E,n,p)
-    return EA, sl
+        x,sl = HEFT(EST,EFT,idx+1,pscheduling.copy(),qlist,T,c,E,n,p,frequency,processorinfo)
+    return EA, EFT[qlist[-1]-1][1]
+
+def DUPRSsecond(EST, EFT, pscheduling, qlist, T, c, E, n, p,frequency,processorinfo,proc_freq):
+    EA=0
+    for idx, i  in reversed(list(enumerate(qlist))):
+        if idx == len(qlist)-1:
+            continue
+        ddl2 = math.inf
+        for j in range(n):
+            if E[j][i-1]==1:
+                ddl2=min(ddl2,EST[j][1]-c[i-1][j])
+                
+        proc = EST[i-1][0]
+        start = EST[i-1][1]
+        ESTcopy = EST.copy()
+        ESTcopy=sorted(ESTcopy,key=lambda x: (x[0],x[1]))
+        for k in ESTcopy:
+            if k[0]==proc and k[1]>start:
+                ddl2=min(ddl2,k[1])
+                break
+        p_energy = math.inf
+        if EFT[i-1][1]<=ddl2:  
+            proc_min = frequency[proc][0]
+            proc_max = frequency[proc][1]
+            stp = (proc_max-proc_min)/100
+            rng = np.arange(proc_min, proc_max, stp)
+            for f in rng:
+                tmp = EST[i-1][1]+T[i-1][proc]*proc_max/f
+                tmp2 = (processorinfo[proc][0]+processorinfo[proc][1]*pow(f,processorinfo[proc][2]))*T[i-1][proc]*proc_max/f
+                if tmp <=ddl2 and tmp2<p_energy:
+                    proc_freq[i-1]=f
+                    p_energy=tmp2
+                    EFT[i-1][1]=tmp
+                    pscheduling[proc]= tmp
+                    break
+            EST[i-1][1]=EFT[i-1][1]-T[i-1][proc]*proc_max/proc_freq[i-1]
+        if(p_energy==math.inf):
+            EA = EA + (processorinfo[proc][0]+processorinfo[proc][1]*pow(proc_freq[i-1],processorinfo[proc][2]))*(T[i-1][proc])*proc_max/proc_freq[i-1]
+        else:
+            EA = EA+p_energy
+    return EA, EFT[qlist[-1]-1][1]
 
 
 if __name__ == "__main__":
@@ -90,18 +134,22 @@ if __name__ == "__main__":
     EST = [[None]*2 for _ in range(n)]
     pscheduling = [0]*p
     
-    sl = HEFT(EST,EFT,0,pscheduling,qlist,T,c,E,n,p)
+    EA,sl = HEFT(EST,EFT,0,pscheduling,qlist,T,c,E,n,p,frequency,processorinfo)
+    print("Phase1: ")
+    print("Energy consumption is: ",EA)
+    print("scheduling length is: ",sl)
     
     proc_freq = [0]*n
     pscheduling = [0]*p
-    
+
     EA, sl = DUPRS1(EST,EFT,0,pscheduling,qlist,T,c,E,n,p,latency,frequency,processorinfo,proc_freq,sl)
-    print(EA)
-    print(sl)
+    print("Phase2: ")
+    print("Energy consumption is: ",EA)
+    print("scheduling length is: ",sl)
+
+    EA, sl = DUPRSsecond(EST,EFT,pscheduling,qlist,T,c,E,n,p,frequency,processorinfo,proc_freq)
+    print("Phase3: ")
+    print("Energy consumption is: ",EA)
+    print("scheduling length is: ",sl)
     
-
-
-    
-
-            
-
+        
